a29305bb292a16525e478bb1196ffd9a864bae2b
Lucas Murakami <lkenjim98@gmail.com>
2020-12-18 12:00:33 -0400

    POST new exam route

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ app.use(cors());
 
 const coursesController = require('./controllers/coursesController');
 const professorsController = require('./controllers/professorsController');
+const examsController = require('./controllers/examsController');
 
 app.get('/api/courses', coursesController.getAll);
 app.get('/api/:courseId/professors', professorsController.getCourseProfessors);
+app.post('/api/new-exam', examsController.postExam);
 
 module.exports = { app };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,9 @@
 const { Course } = require('./Course');
 const { Professor } = require('./Professor');
+const { Exam } = require('./Exam')
 
 module.exports = {
     Course,
-    Professor
+    Professor,
+    Exam
 };
