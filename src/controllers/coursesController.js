b8c004884408a9e8862526d40e7fc51a742167a3
Lucas Murakami <lkenjim98@gmail.com>
2020-12-18 00:28:46 -0400

    Handling errors

--- src/controllers/coursesController.js
+++ src/controllers/coursesController.js
@@ -1,7 +1,13 @@
 const { Course } = require('../utils');
 
 async function getAll(req, res) {
-    const courses = await Course.getCourses();
+    let courses;
+    try {
+        courses = await Course.getCourses();
+    }
+    catch {
+        return res.status(500).send('Erro interno no servidor');
+    }
 
     return res.status(200).send(courses);
 }
--- src/controllers/professorsController.js
+++ src/controllers/professorsController.js
@@ -3,7 +3,13 @@ const { Professor } = require('../utils');
 async function getCourseProfessors(req, res) {
     const { courseId } = req.params;
 
-    const professors = await Professor.getProfessorsByCourseId(courseId);
+    let professors;
+    try {
+        professors = await Professor.getProfessorsByCourseId(courseId);
+    }
+    catch {
+        return res.status(500).send('Erro interno no servidor');
+    }
 
     return res.status(200).send(professors);
 }
